095551e3df88fa20957b48636b3f954ab126887a
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-21 22:43:14 -0600

    first commit. htm/css mostly set.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const inputNum = document.getElementById('characterAmountNumber')
+
+const inputRange = document.getElementById('characterAmountRange')
+
+console.log(inputNum.value)
+
+if (inputRange.value === inputNum.value) {
+  console.log('working')
+} else {
+}
