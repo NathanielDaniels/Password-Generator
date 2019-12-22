49340f66b23c7e43e8f003b0656853d93b6bb0a8
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-22 12:42:37 -0600

    added form preventDefault + changed syncChar.. to func declaration

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const characterNumber = document.getElementById('characterAmountNumber')
 const characterRange = document.getElementById('characterAmountRange')
-
-syncCharacterAmount = e => {
-  const value = e.target.value
-  characterAmountNumber.value = value
-  characterAmountRange.value = value
-}
+const form = document.getElementById('passwordGeneratorForm')
 
 characterNumber.addEventListener('input', syncCharacterAmount)
 characterRange.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+})
+
+function syncCharacterAmount (event) {
+  const value = event.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
