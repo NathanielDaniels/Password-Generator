effbd01ed36a45ca5208f1a78977b9daeec257c4
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-22 23:54:21 -0600

    working on JS for inputs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 const characterNumber = document.getElementById('characterAmountNumber')
 const characterRange = document.getElementById('characterAmountRange')
 const form = document.getElementById('passwordGeneratorForm')
+const includeUppercase = document.getElementById('includeUppercase')
+const includeNumbers = document.getElementById('includeNumbers')
+const includeSymbols = document.getElementById('includeSymbols')
 
 characterNumber.addEventListener('input', syncCharacterAmount)
 characterRange.addEventListener('input', syncCharacterAmount)
+includeUppercase.addEventListener('input')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  )
 })
 
 function syncCharacterAmount (event) {
