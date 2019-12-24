e116bc83eed9f38be5464807a95c3dcbf5f34995
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-23 23:56:39 -0600

    adding events to button click event listener

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-const characterNumber = document.getElementById('characterAmountNumber')
-const characterRange = document.getElementById('characterAmountRange')
-const form = document.getElementById('passwordGeneratorForm')
-const includeUppercase = document.getElementById('includeUppercase')
-const includeNumbers = document.getElementById('includeNumbers')
-const includeSymbols = document.getElementById('includeSymbols')
+const characterNumber = document.getElementById("characterAmountNumber");
+const characterRange = document.getElementById("characterAmountRange");
+const form = document.getElementById("passwordGeneratorForm");
+const includeUppercase = document.getElementById("includeUppercase");
+const includeNumbers = document.getElementById("includeNumbers");
+const includeSymbols = document.getElementById("includeSymbols");
 
-characterNumber.addEventListener('input', syncCharacterAmount)
-characterRange.addEventListener('input', syncCharacterAmount)
-includeUppercase.addEventListener('input')
+characterNumber.addEventListener("input", syncCharacterAmount);
+characterRange.addEventListener("input", syncCharacterAmount);
+includeUppercase.addEventListener("input");
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const characterAmount = characterAmountNumber.value;
+  const includeUppercase = includeUppercase.checked;
   const password = generatePassword(
     characterAmount,
     includeUppercase,
     includeNumbers,
     includeSymbols
-  )
-})
+  );
+});
 
-function syncCharacterAmount (event) {
-  const value = event.target.value
-  characterAmountNumber.value = value
-  characterAmountRange.value = value
+function syncCharacterAmount(event) {
+  const value = event.target.value;
+  characterAmountNumber.value = value;
+  characterAmountRange.value = value;
 }
